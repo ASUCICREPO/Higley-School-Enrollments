12f85fabf9d19750a9f2921179674da873348d83
Anjali Srivastava <anjali.pihu@gmail.com>
2023-05-16 15:28:36 -0700

    Upload Replace docs

--- src/pages/ReplaceDocs.js
+++ src/pages/ReplaceDocs.js
@@ -1,11 +1,30 @@
 import React from "react";
 // import { Firsttimeuser, Rectangle35 } from "../ui-components";
-import { Replacedocs } from '../ui-components';
+import { Component23, Component24, Component25, Component26, Component27, Component28, Component29, Component30, Component31, Component32, Component33, Component34, Replacedocs } from '../ui-components';
 
 const ReplaceDocs = () => {
     return(
         <>
-            <Replacedocs width='100%' />
+            <div style={{display: 'flex', width: '100%', marginTop: '100px'}}>
+                <div style={{width: '50%'}}>
+                    <Component23 />
+                    <Component24 />
+                    <Component25 />
+                    <Component26 />
+                    <Component27 />
+                    <Component28 />
+                </div>
+                <div style={{width: '50%'}}>
+                    <Component29 />
+                    <Component30 />
+                    <Component31 />
+                    <Component32 />
+                    <Component33 />
+                    <Component34 />
+
+                </div>
+            </div>
+            {/* <Replacedocs width='100%' /> */}
         </>
     )
 
--- src/pages/UploadDocs.js
+++ src/pages/UploadDocs.js
@@ -1,26 +1,42 @@
 import React from "react";
 // import { Firsttimeuser, Rectangle35 } from "../ui-components";
 import { Component11, Component12, Component13, Component14, Component15, Component16, Component17, Component18, Component19, Component20, Component21, Component22, Uploaddocs } from '../ui-components';
+import { BirthrateGDPUScsv, EnrollmentGradescsv, Component41, Enrollmentcountscsv, Housingpopulationcsv, Landdevelopmentscsv, Personaddresshistorycsv, Studentactivitiescsv, Studentattendancecsv, StudentBenchmarkscsv, StudentDemographicscsv, Studentenrollmentscsv, Schoollunchcsv } from '../ui-components';
+import SchoolLunchcsv from "../ui-components/Schoollunchcsv";
 
 const UploadDocs = () => {
     return(
         <>
             <div style={{display: 'flex', width: '100%', marginTop: '100px'}}>
                 <div style={{width: '50%'}}>
-                    <Component11 />
-                    <Component12 />
-                    <Component13 />
-                    <Component14 />
-                    <Component15 />
-                    <Component16 />
+                    <BirthrateGDPUScsv />
+                    <Component41 />
+                    <Enrollmentcountscsv />
+                    <Component41 />
+                    <EnrollmentGradescsv />
+                    <Component41 />
+                    <Housingpopulationcsv />
+                    <Component41 />
+                    <Landdevelopmentscsv />
+                    <Component41 />
+                    <SchoolLunchcsv />
+                    <Component41 />
+                    <Schoollunchcsv />
+                    <Component41 />
                 </div>
                 <div style={{width: '50%'}}>
-                    <Component17 />
-                    <Component18 />
-                    <Component19 />
-                    <Component20 />
-                    <Component21 />
-                    <Component22 />
+                    <Personaddresshistorycsv />
+                    <Component41 />
+                    <Studentactivitiescsv />
+                    <Component41 />
+                    <Studentattendancecsv />
+                    <Component41 />
+                    <StudentBenchmarkscsv />
+                    <Component41 />
+                    <StudentDemographicscsv />
+                    <Component41 />
+                    <Studentenrollmentscsv />
+                    <Component41 />
 
                 </div>
             </div>
