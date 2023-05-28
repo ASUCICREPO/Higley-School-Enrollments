f08831facdd41dbba6e658550b752b24f8f42064
Anjali Srivastava <anjali.pihu@gmail.com>
2023-05-28 04:57:10 -0700

    handle event on click

--- src/pages/ReturnUser.js
+++ src/pages/ReturnUser.js
@@ -1,14 +1,20 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+
 import { Component3, Component4, Component6 } from '../ui-components';
 
 
 const ReturnUser = () => {
+    const navigate = useNavigate();
+    function handleClick(event) {
+        navigate('/replacedocs');
+    }
     return(
         <>
             <div style={{display: 'flex', width: '100%', marginTop: '100px'}}>
                 <div style={{width: '50%'}}>
                     <Component6 />
-                    <Component4 />
+                    <Component4 onClick={handleClick}/>
                 </div>
                 <div style={{width: '50%'}}>
                     <Component3 />
--- src/pages/UserDetails.js
+++ src/pages/UserDetails.js
@@ -1,15 +1,21 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
+
 // import { Firsttimeuser, Rectangle35 } from "../ui-components";
-import { Component3, Component4, Component5, Firsttimeuser } from '../ui-components';
+import { Component3, Component4, Component5 } from '../ui-components';
 
 const UserDetails = () => {
+    const navigate = useNavigate();
+    function handleClick(event) {
+        navigate('/uploaddocs');
+    }
     return(
         <>
             {/* <Firsttimeuser /> */}
             <div style={{display: 'flex', width: '100%', marginTop: '100px'}}>
                 <div style={{width: '50%'}}>
                     <Component5 />
-                    <Component4 />
+                    <Component4 onClick={handleClick}/>
                 </div>
                 <div style={{width: '50%'}}>
                     <Component3 />
